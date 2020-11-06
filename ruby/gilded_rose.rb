0fceaadb52a12c53525c6dbe99a282bb8a5b7d0c
Lito White <leland.white@gmail.com>
2020-11-06 08:10:05 -0700

    Move item types into their own classes.

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -6,34 +6,77 @@ class GildedRose
 
   def update_quality
     @items.each do |item|
-      next if item.name == "Sulfuras, Hand of Ragnaros"
-      if item.name == 'Aged Brie'
-        item.sell_in -= 1
-        item.quality += 1 unless item.quality >= 50
+      if item.name == "Sulfuras, Hand of Ragnaros"
+        Sulfuras.new(item).update_quality
+      elsif item.name == 'Aged Brie'
+        AgedBrie.new(item).update_quality
       elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
-        item.sell_in -= 1
-        item.quality += 1 unless item.quality >= 50
-        if item.sell_in < 10
-          item.quality += 1 unless item.quality >= 50
-        end
-        if item.sell_in < 5
-          item.quality += 1 unless item.quality >= 50
-        end
-        if item.sell_in < 0
-          item.quality = 0
-        end
+        Backstage.new(item).update_quality
       else
-        item.sell_in -= 1
-        item.quality -= 1 if item.quality > 0
-        if item.sell_in < 0
-          item.quality -= 1 if item.quality > 0 
-        end
+        Generic.new(item).update_quality
       end
     end
   end
 end
 
+class Sulfuras
+  attr_reader :item
+  def initialize(item)
+    @item = item
+  end
 
+  def update_quality
+    item
+  end
+end
+
+class AgedBrie
+  attr_reader :item
+  def initialize(item)
+    @item = item
+  end
+
+  def update_quality
+    item.sell_in -= 1
+    item.quality += 1 unless item.quality >= 50
+  end
+end
+
+class Generic
+  attr_reader :item
+  def initialize(item)
+    @item = item
+  end
+
+  def update_quality
+    item.sell_in -= 1
+    item.quality -= 1 if item.quality > 0
+    if item.sell_in < 0
+      item.quality -= 1 if item.quality > 0
+    end
+  end
+end
+
+class Backstage
+  attr_reader :item
+  def initialize(item)
+    @item = item
+  end
+
+  def update_quality
+    item.sell_in -= 1
+    item.quality += 1 unless item.quality >= 50
+    if item.sell_in < 10
+      item.quality += 1 unless item.quality >= 50
+    end
+    if item.sell_in < 5
+      item.quality += 1 unless item.quality >= 50
+    end
+    if item.sell_in < 0
+      item.quality = 0
+    end
+  end
+end
 
 class Item
   attr_accessor :name, :sell_in, :quality
